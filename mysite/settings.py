dae3f06b4250e936dc620964ae139b0bd9efde70
Mengjia Kang <mengjiakang2017@u.northwestern.edu>
2018-09-27 15:18:01 -0500

    Added url to allowed_urls in mysite/settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'zeasn^=fu#79zowqo)7^imd-3z6q)_hw8&3#qs)h%2@qcp%0nu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1:8000', '.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
